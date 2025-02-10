956065d8b4f11a8706749e1202baf4e5e14e987b
Kirill Medvedev <repkomail@gmail.com>
2025-02-10 21:35:00 +0200

    fix: add charts components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Capital from './components/Capital/Capital'
 import Bots from './components/Bots/Bots'
 import Time from './components/Time/Time'
 import Footer from './components/Footer/footer'
+import Chart from './components/Chart/Chart'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
     <>
       <Header></Header>
       <Capital></Capital>
+      <Chart></Chart>
       <Bots></Bots>
       <Time></Time>
       <Footer></Footer>
